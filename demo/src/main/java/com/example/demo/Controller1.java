bc1c01670be3cda5521e08dda007963141dc6761
ishibashishunsuke <ishibashi.shun@icloud.com>
2024-09-11 19:59:41 +0900

    9/11

--- demo/src/main/java/com/example/demo/Controller1.java
+++ demo/src/main/java/com/example/demo/Controller1.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @Controller
 public class Controller1 {
+	//@Autowired
+	//private TaskdbRepository taskdbRepository;
+	
 	
 	String message = null;
 	String message2 = null;
 	String message3 = null;
+	String message4 = null;
 	
 	@GetMapping("/menu")
    public String menu(){
@@ -21,57 +26,56 @@ public class Controller1 {
 	}
 	
 	
-	@GetMapping("/send")
-		public String send() {
-		return "send";
-	}
 	
+	@RequestMapping("/send01")
+    public String send01(){
+        return "send01";
+    }
+
 	
-	@PostMapping("/receive")
-	public String receive(
-		Model m,
-		@RequestParam("syain") String syain,
-		@RequestParam("age") String age,
-		@RequestParam("pass") String pass,
-		@RequestParam("pass2") String pass2
-	) {
-		if(syain.length() > 0) {
-		m.addAttribute("syain",syain);
-		}else {
-			message = "必須入力です";
-			return "send";
-		}
-		
-		if(age.length() > 0) {
-		m.addAttribute("age",age);
-		}else {
-			message = "必須入力です";
-			return "send";
-		}
-		
-		if((pass.length() > 0) && (pass.matches("[a-zA-Z0-9]{8,}"))) {
-		m.addAttribute("pass",pass);
-		}else {
-			message = "必須入力です";
-			message2 = "大小半角英数を含む8文字以上で入力してください";
-			return "send";
-		}
-		
-		if(pass2.length() > 0 && (pass2.matches("[a-zA-Z0-9]{8,}")) && (pass == pass2)) {
-		m.addAttribute("pass2",pass2);
-		}else {
-			message = "必須入力です";
-			message2 = "大小半角英数を含む8文字以上で入力してください";
-			message3 = "パスワードと同じものを入力してください";
-			return "send";
-		}
-			
-	    return "receive";
+    @PostMapping("/receive01")
+    public String receive01(
+        Model m,
+        @RequestParam("syain")String syain,
+        @RequestParam("age")String age,
+        @RequestParam("pass")String pass,
+        @RequestParam("pass2")String pass2
+    ){
+    	if(syain.length()== 0 || syain==null){
+        		message = "社員名を入力してください";
+        		m.addAttribute("message", message);
+        	
+        	}else if(age.length()== 0 || age==null) {
+        		message2 = "年齢を入力してください";
+        		m.addAttribute("message2", message2);
+        	}else if(pass==null || !pass.matches("[0-9a-zA-Z]{8,}")) {
+        		message3 = "大小半角英数を含む8文字以上で入力してください";
+        		m.addAttribute("message3", message3);
+        		
+        	}else if(!pass.matches(pass2)) {
+        		message4 = "パスワードと同じものを入力してください";
+        		m.addAttribute("message4", message4);
+        		
+        	}else {
+        		m.addAttribute("syain", syain);
+                m.addAttribute("age", age);
+                m.addAttribute("pass", pass);
+                m.addAttribute("pass2", pass2);
+                return "receive01";
+        	}
+    	
+    	return "send01";
     }
 	
 	
 	@PostMapping("/finish")
-	public String finish() {
+	public String finish(
+	) {
+		
+		
+		
+		//taskdbRepository.saveAndFlush(taskdb);
+		
 	return "finish";
     }
 	
@@ -84,18 +88,22 @@ public class Controller1 {
 	String str = "Aa123456";
 	String str2 = "Aa123456";
 	String str3= null;
-	if((str.length() > 0) && (str.matches("[a-zA-Z0-9]{8,}")) ) {
+	if((str.length() > 0) && (!str.matches("[0-9a-zA-Z]{8,}")) ) {
 		System.out.println(str);
 		 str3 = "あああ";
 		 System.out.println(str3);
 	}else {
 		System.out.println("aaa");
 	}
-	if((str2.length() > 0) && (str2.matches("[a-zA-Z0-9]{8,}")) && (str==str2) ) {
+	if(!str.matches(str2)) {
 		System.out.println(str2);
 	}else {
 		System.out.println("aaa");
 	}
 	
 	}
+	
+	
+	
+    
 }
